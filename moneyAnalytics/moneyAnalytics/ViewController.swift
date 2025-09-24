95ea67ddb3cb9f1fadd9937591a585a3101728cf
Fairim <rus.ahmetsafin@bk.ru>
2025-09-24 19:20:02 +0300

    Добавил все элементы на экран для дальнейшей разработки главной страницы

--- moneyAnalytics/moneyAnalytics/ViewController.swift
+++ moneyAnalytics/moneyAnalytics/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var spendingButtom: UIButton!
+    @IBOutlet weak var incomeButtom: UIButton!
+    @IBOutlet weak var MoneyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var WeekDay: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickSpendingButtom(_ sender: Any) {
+        //Запускается страница с добавлением расхода
+    }
+    
+    @IBAction func clickIncomeButtom(_ sender: Any) {
+        //Запускается страница с добавлением расхода
+    }
+    
 }
 
