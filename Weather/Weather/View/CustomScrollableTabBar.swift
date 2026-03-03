40505e9c9d102aa13af6fed70c0c66c186cda353
Fairim <jorgenbo@mail.ru>
2026-03-03 10:54:21 +0300

    Необходимо реализовать через scrollTabBar а не просто TabBar

---
+++ Weather/Weather/View/CustomScrollableTabBar.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class CustomScrollableTabBar: UIView {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = 20
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
+        cv.backgroundColor = .clear
+        return cv
+    }()
+
+    
+}
